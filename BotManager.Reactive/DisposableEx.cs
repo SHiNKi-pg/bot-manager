c1a0a0b952dd6a3891fdeb7ce3769afc339d8795
SHiNKi <shinki.5ktri@gmail.com>
2023-11-18 20:18:49 +0900

    DisposableExクラス追加

---
+++ BotManager.Reactive/DisposableEx.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotManager.Reactive
+{
+    /// <summary>
+    /// <see cref="IDisposable"/>拡張メソッド定義用クラス
+    /// </summary>
+    public static class DisposableEx
+    {
+    }
+}
