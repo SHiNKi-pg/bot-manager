408d8f9a26dedeb5232d0e5253ee617d55e5d7d3
SHiNKi <shinki.5ktri@gmail.com>
2023-11-18 15:18:47 +0900

    Botインターフェース

---
+++ BotManager.Common/IBotManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotManager.Common
+{
+    /// <summary>
+    /// Botマネージャーインターフェース
+    /// </summary>
+    public interface IBotManager : IDisposable, IStartEnd
+    {
+        /// <summary>
+        /// Bot
+        /// </summary>
+        IEnumerable<IBot> Bots { get; }
+
+        /// <summary>
+        /// 指定したIDの<see cref="IBot"/>があればそれを返します。
+        /// </summary>
+        /// <param name="id">BotID</param>
+        /// <param name="bot">Bot</param>
+        /// <returns></returns>
+        bool TryGetBot(string id, [NotNullWhen(true)] out IBot? bot);
+
+    }
+}
