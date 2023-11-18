5286e2c833e8d72d5cca225d00560e7e19b18a92
SHiNKi <shinki.5ktri@gmail.com>
2023-11-18 20:14:47 +0900

    IDiscordServiceClientインターフェース追加

---
+++ BotManager.Service.Discord/IDiscordServiceClient.cs
@@ -0,0 +1,16 @@
+﻿using BotManager.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotManager.Service.Discord
+{
+    /// <summary>
+    /// Bot用Discordクライアント
+    /// </summary>
+    public interface IDiscordServiceClient : IServiceClient
+    {
+    }
+}
