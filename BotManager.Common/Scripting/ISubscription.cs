af8aa4400950e8c942c619c709c864876f89ce76
SHiNKi <shinki.5ktri@gmail.com>
2023-11-19 00:32:29 +0900

    ISubscriptionインターフェース追加

---
+++ BotManager.Common/Scripting/ISubscription.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotManager.Common.Scripting
+{
+    /// <summary>
+    /// サブスクリプションインターフェース
+    /// </summary>
+    public interface ISubscription
+    {
+        /// <summary>
+        /// イベントを購読します。
+        /// </summary>
+        /// <param name="args">通知元のオブジェクト等を含むオブジェクト</param>
+        /// <returns></returns>
+        IDisposable SubscribeFrom(ISubscriptionArguments args);
+    }
+}
