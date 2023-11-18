4dbe7158e1ca13c93f7e80fe6e8d2ba1b449d186
SHiNKi <shinki.5ktri@gmail.com>
2023-11-18 20:24:57 +0900

    DisposeOn拡張メソッド追加

--- BotManager.Reactive/DisposableEx.cs
+++ BotManager.Reactive/DisposableEx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,5 +12,18 @@ namespace BotManager.Reactive
     /// </summary>
     public static class DisposableEx
     {
+        /// <summary>
+        /// 指定したストリームからデータが通知された場合に このオブジェクトの<seealso cref="IDisposable.Dispose"/>を呼び出すように登録します。
+        /// </summary>
+        /// <typeparam name="TSignal"></typeparam>
+        /// <param name="disposable"></param>
+        /// <param name="observable">このオブジェクトの <see cref="IDisposable.Dispose"/>を呼び出すタイミング</param>
+        /// <returns><see cref="DisposeOn{TSignal}(IDisposable, IObservable{TSignal})"/>で登録した破棄予約を解除するためのオブジェクト。</returns>
+        public static IDisposable DisposeOn<TSignal>(this IDisposable disposable, IObservable<TSignal> observable)
+        {
+            return observable
+                .Take(1)
+                .Subscribe(_ => disposable.Dispose());
+        }
     }
 }
