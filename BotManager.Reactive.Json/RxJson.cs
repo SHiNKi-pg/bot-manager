cc255ba297e2416aa7524e1fabc8c6ef24f41576
SHiNKi <shinki.5ktri@gmail.com>
2023-11-27 21:14:45 +0900

    Reactive.Jsonプロジェクト作成

---
+++ BotManager.Reactive.Json/RxJson.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotManager.Reactive.Json
+{
+    /// <summary>
+    /// リアクティブJSON拡張メソッド定義クラス
+    /// </summary>
+    public static class RxJson
+    {
+        
+    }
+}
