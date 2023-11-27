c6e3b10d50f1229976efe33aadd8a4f419b170be
SHiNKi <shinki.5ktri@gmail.com>
2023-11-27 23:54:24 +0900

    IMisskeyEventNotifierインターフェース メソッド追加

--- BotManager.Service.Misskey/IMisskeyEventNotifier.cs
+++ BotManager.Service.Misskey/IMisskeyEventNotifier.cs
@@ -1,4 +1,5 @@
 ﻿using BotManager.Common;
+using BotManager.Service.Misskey.Schemas;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,5 +13,32 @@ namespace BotManager.Service.Misskey
     /// </summary>
     public interface IMisskeyEventNotifier : IEventNotifier
     {
+        /// <summary>
+        /// グローバルタイムラインを取得します。
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        IObservable<Note> GetGlobalTimeline(string id);
+
+        /// <summary>
+        /// ホームタイムラインを取得します。
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        IObservable<Note> GetHomeTimeline(string id);
+
+        /// <summary>
+        /// ソーシャルタイムラインを取得します。
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        IObservable<Note> GetHybridTimeline(string id);
+
+        /// <summary>
+        /// ローカルタイムラインを取得します。
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        IObservable<Note> GetLocalTimeline(string id);
     }
 }
