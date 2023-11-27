065a09c3e4fb0b72729746ce6db3725f054ccaf3
SHiNKi <shinki.5ktri@gmail.com>
2023-11-27 21:22:10 +0900

    RxJson.WhereIs 拡張メソッド 追加

--- BotManager.Reactive.Json/RxJson.cs
+++ BotManager.Reactive.Json/RxJson.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,6 +13,35 @@ namespace BotManager.Reactive.Json
     /// </summary>
     public static class RxJson
     {
-        
+        /// <summary>
+        /// JSON文字列を .NET オブジェクトに変換し、後続へ通知します。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="jsonStrings">JSON文字列が通知されるストリーム</param>
+        /// <returns></returns>
+        public static IObservable<T> WhereIs<T>(this IObservable<string> jsonStrings)
+        {
+            return Observable.Create<T>(observer =>
+            {
+                return jsonStrings.Subscribe(jsonString =>
+                {
+                    try
+                    {
+                        var json = JsonConvert.DeserializeObject<T>(jsonString);
+                        if (json is T t)
+                        {
+                            // JSON文字列を変換した結果、T型なら後続に通知する。
+                            observer.OnNext(t);
+                        }
+                    }catch(Exception ex)
+                    {
+                        // 変換等で何らかエラーが起きた場合は後続に通知
+                        observer.OnError(ex);
+                    }
+                },
+                observer.OnError,
+                observer.OnCompleted);
+            });
+        }
     }
 }
