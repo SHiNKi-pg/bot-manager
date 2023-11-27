20c3d68d8442577f741ea6acea6978442aec4528
SHiNKi <shinki.5ktri@gmail.com>
2023-11-27 21:23:21 +0900

    RxJson.WhereIs メソッド オーバーロード

--- BotManager.Reactive.Json/RxJson.cs
+++ BotManager.Reactive.Json/RxJson.cs
@@ -43,5 +43,38 @@ namespace BotManager.Reactive.Json
                 observer.OnCompleted);
             });
         }
+
+        /// <summary>
+        /// JSON文字列を .NET オブジェクトに変換し、それが指定した条件を満たす場合に後続へ通知します。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="jsonStrings">JSON文字列が通知されるストリーム</param>
+        /// <param name="predicate">条件</param>
+        /// <returns></returns>
+        public static IObservable<T> WhereIs<T>(this IObservable<string> jsonStrings, Func<T, bool> predicate)
+        {
+            return Observable.Create<T>(observer =>
+            {
+                return jsonStrings.Subscribe(jsonString =>
+                {
+                    try
+                    {
+                        var json = JsonConvert.DeserializeObject<T>(jsonString);
+                        if (json is T t && predicate(t))
+                        {
+                            // JSON文字列を変換した結果、T型なら後続に通知する。
+                            observer.OnNext(t);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        // 変換等で何らかエラーが起きた場合は後続に通知
+                        observer.OnError(ex);
+                    }
+                },
+                observer.OnError,
+                observer.OnCompleted);
+            });
+        }
     }
 }
