53dc65d6087a56ff12c12a8a6b305c225191bafb
SHiNKi <shinki.5ktri@gmail.com>
2023-11-27 23:00:29 +0900

    ISendableTimelineインターフェース追加

---
+++ BotManager.Service.Misskey/ISendableTimeline.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotManager.Service.Misskey
+{
+    /// <summary>
+    /// 送信可能タイムライン
+    /// </summary>
+    public interface ISendableTimeline
+    {
+        /// <summary>
+        /// データを送信します
+        /// </summary>
+        /// <typeparam name="TBody"></typeparam>
+        /// <param name="body"></param>
+        void Send<TBody>(TBody body);
+    }
+
+    /// <summary>
+    /// 汎用タイムラインインターフェース
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IGenericTimeline<out T> : ISendableTimeline, IObservable<T>
+    {
+
+    }
+}
