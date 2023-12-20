050a909928932273d327d9aaa1130c61e2bfdbb5
SHiNKi <shinki.5ktri@gmail.com>
2023-12-21 00:36:46 +0900

    エンドポイントで分ける用のベースクラス(MisskeyApiBase)を追加 PostAsyncメソッドの定義場所を変更

---
+++ BotManager.Service.Misskey/Api/MisskeyApiBase.cs
@@ -0,0 +1,61 @@
+﻿using BotManager.Common.Web;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotManager.Service.Misskey.Api
+{
+    /// <summary>
+    /// MisskeyAPIベースクラス（エンドポイント別）
+    /// </summary>
+    public abstract class MisskeyApiBase
+    {
+        #region Private Fields
+        private MisskeyApi misskeyApi;
+        #endregion
+
+        #region Constractor
+        internal MisskeyApiBase(MisskeyApi misskeyApi)
+        {
+            this.misskeyApi = misskeyApi;
+        }
+        #endregion
+
+        #region Base Method
+        /// <summary>
+        /// POSTリクエストを送信します。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="endPoint">エンドポイント名</param>
+        /// <param name="requestBody">リクエストボディ</param>
+        /// <returns></returns>
+        /// <exception cref="HttpRequestException"></exception>
+        internal protected async Task<T?> PostAsync<T>(string endPoint, Dictionary<string, object?> requestBody)
+        {
+            string url = misskeyApi.BaseUrl + endPoint;
+
+            // 認証情報の付与
+            requestBody.Add("i", misskeyApi.AccessToken);
+            requestBody.Add("detail", false);
+
+            string json = JsonConvert.SerializeObject(requestBody);
+            using (var content = new StringContent(json, Encoding.UTF8, "application/json"))
+            {
+                var response = await HttpClientSingleton.HttpClient.PostAsync(url, content);
+                if (!response.IsSuccessStatusCode)
+                {
+                    // エラー系のステータスコードの場合は例外を投げる
+                    throw new HttpRequestException($"[{url}] Status Code : {(int)response.StatusCode}", null, statusCode: response.StatusCode);
+                }
+                string responseString = await response.Content.ReadAsStringAsync();
+
+                var jsonObj = JsonConvert.DeserializeObject<T>(responseString);
+                return jsonObj;
+            }
+        }
+        #endregion
+    }
+}
--- BotManager.Service.Misskey/MisskeyApi.cs
+++ BotManager.Service.Misskey/MisskeyApi.cs
@@ -39,38 +39,16 @@ namespace BotManager.Service.Misskey
         }
         #endregion
 
-        #region Base Method
+        #region Property
         /// <summary>
-        /// POSTリクエストを送信します。
+        /// ベースURL
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="endPoint">エンドポイント名</param>
-        /// <param name="requestBody">リクエストボディ</param>
-        /// <returns></returns>
-        /// <exception cref="HttpRequestException"></exception>
-        internal protected async Task<T?> PostAsync<T>(string endPoint, Dictionary<string, object?> requestBody)
-        {
-            string url = baseUrl + endPoint;
+        public string BaseUrl { get => baseUrl; }
 
-            // 認証情報の付与
-            requestBody.Add("i", accessToken);
-            requestBody.Add("detail", false);
-
-            string json = JsonConvert.SerializeObject(requestBody);
-            using (var content = new StringContent(json, Encoding.UTF8, "application/json"))
-            {
-                var response = await HttpClientSingleton.HttpClient.PostAsync(url, content);
-                if (!response.IsSuccessStatusCode)
-                {
-                    // エラー系のステータスコードの場合は例外を投げる
-                    throw new HttpRequestException($"[{url}] Status Code : {(int)response.StatusCode}", null, statusCode: response.StatusCode);
-                }
-                string responseString = await response.Content.ReadAsStringAsync();
-
-                var jsonObj = JsonConvert.DeserializeObject<T>(responseString);
-                return jsonObj;
-            }
-        }
+        /// <summary>
+        /// アクセストークン
+        /// </summary>
+        internal string AccessToken { get => accessToken; }
         #endregion
     }
 }
