92dba09de15e64638b3eb328e6aa9a57824c0c4e
SHiNKi <shinki.5ktri@gmail.com>
2024-01-04 20:25:40 +0900

    Twitter Bot設定取得テスト追加

--- Twitter.Tests/Tweets.cs
+++ Twitter.Tests/Tweets.cs
@@ -21,6 +21,25 @@ namespace Twitter.Tests
             client = new(certification.ConsumerKey, certification.ConsumerSecret, certification.AccessToken, certification.AccessTokenSecret);
         }
 
+        [Fact(DisplayName = "Bot設定取得")]
+        public void GetBotsTest()
+        {
+            var bots = AppSettings.Bots;
+            foreach (var bot in bots)
+            {
+                output.WriteLine("BotId : {0}", bot.Id);
+                var twitter = bot.TwitterSetting;
+                if (twitter != null)
+                {
+                    output.WriteLine("ConsumerKey : {0}", twitter.Certificate.ConsumerKey);
+                    output.WriteLine("ConsumerSecret : {0}", twitter.Certificate.ConsumerSecret);
+                    output.WriteLine("AccessToken : {0}", twitter.Certificate.AccessToken);
+                    output.WriteLine("AccessTokenSecret : {0}", twitter.Certificate.AccessTokenSecret);
+                    output.WriteLine("");
+                }
+            }
+        }
+
         public void Dispose()
         {
             client.Dispose();
