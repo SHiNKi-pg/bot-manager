3490e6a7c73bfebc773f3790a34b95df7550b952
SHiNKi <shinki.5ktri@gmail.com>
2024-01-04 20:22:30 +0900

    Twitter用テストプロジェクト追加

---
+++ Twitter.Tests/Tweets.cs
@@ -0,0 +1,29 @@
+﻿using BotManager.Engine;
+using BotManager.Service.Twitter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace Twitter.Tests
+{
+    public class Tweets : IDisposable
+    {
+        private readonly TwitterServiceClient client;
+        private readonly ITestOutputHelper output;
+
+        public Tweets(ITestOutputHelper output)
+        {
+            var certification = AppSettings.GetBotDictionary()["test"].TwitterSetting?.Certificate!;
+            this.output = output;
+            client = new(certification.ConsumerKey, certification.ConsumerSecret, certification.AccessToken, certification.AccessTokenSecret);
+        }
+
+        public void Dispose()
+        {
+            client.Dispose();
+        }
+    }
+}
