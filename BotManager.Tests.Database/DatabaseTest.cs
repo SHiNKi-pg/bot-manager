236c23bda5bf713b4a2be73f91d66786ef5c189b
SHiNKi <shinki.5ktri@gmail.com>
2024-01-06 18:56:08 +0900

    データベーステストプロジェクト追加

---
+++ BotManager.Tests.Database/DatabaseTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace BotManager.Tests.Database
+{
+    public class DatabaseTest
+    {
+        private readonly ITestOutputHelper output;
+
+        public DatabaseTest(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        [Fact(DisplayName = "データベース接続")]
+        public async Task ConnectionTest()
+        {
+            using(var db = BotManager.Database.Database.Connect())
+            {
+                using(var tran = await db.BeginTransactionAsync())
+                {
+                    output.WriteLine("トランザクション開始");
+                    await Task.Delay(3000);
+
+                    tran.Commit();
+                    output.WriteLine("トランザクション コミット");
+                }
+                output.WriteLine("トランザクション終了");
+            }
+        }
+    }
+}
