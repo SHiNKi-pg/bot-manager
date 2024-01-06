7e02d2356a8879d636698472bdd1b4dcf0f059e1
SHiNKi <shinki.5ktri@gmail.com>
2024-01-07 01:50:20 +0900

    スクリプト用属性クラス追加

---
+++ BotManager.Common/Scripting/Attributes/BotAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotManager.Common.Scripting.Attributes
+{
+    /// <summary>
+    /// このクラスがBot定義クラスであることを示します。
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public class BotAttribute : Attribute
+    {
+    }
+}
