41bcea05c607d1d7eaa558b6d02504fba326162c
SHiNKi <shinki.5ktri@gmail.com>
2024-01-06 13:41:16 +0900

    Gitクラス追加

---
+++ BotManager.Service.Git/Git.cs
@@ -0,0 +1,26 @@
+﻿using LibGit2Sharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotManager.Service.Git
+{
+    /// <summary>
+    /// Gitコマンドクラス
+    /// </summary>
+    public static class Git
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="repositry">リポジトリのURLまたはパス</param>
+        /// <param name="path">クローン先のディレクトリ</param>
+        /// <returns></returns>
+        public static IGitRepositry Clone(string repositry, string path)
+        {
+            return new GitRepository(Repository.Clone(repositry, path));
+        }
+    }
+}
---
+++ BotManager.Service.Git/GitRepository.cs
@@ -0,0 +1,37 @@
+﻿using LibGit2Sharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotManager.Service.Git
+{
+    /// <summary>
+    /// Gitリポジトリインターフェース
+    /// </summary>
+    public interface IGitRepositry : IDisposable
+    {
+
+    }
+    internal sealed class GitRepository : IGitRepositry
+    {
+        #region Private Fields
+        private Repository repository;
+        #endregion
+
+        #region Constructor
+        public GitRepository(string repositryPath)
+        {
+            repository = new Repository(repositryPath);
+        }
+        #endregion
+
+        #region Method
+        public void Dispose()
+        {
+            repository.Dispose();
+        }
+        #endregion
+    }
+}
