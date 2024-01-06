3af688ead4830b9bdf51c5780c6921d84aaea7a4
SHiNKi <shinki.5ktri@gmail.com>
2024-01-07 02:39:24 +0900

    Bot実行機関クラス追加

---
+++ BotManager.Engine/BotMechanism.cs
@@ -0,0 +1,40 @@
+﻿using BotManager.Common;
+using BotManager.Service.Compiler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotManager.Engine
+{
+    /// <summary>
+    /// BotManager 実行機関クラス
+    /// </summary>
+    internal sealed class BotMechanism : IBotMechanism
+    {
+        #region Private Fields
+        private ICompiler compiler;
+        private IBotManager? _botManager;
+        #endregion
+
+        #region Constructor
+        public BotMechanism(string assemblyName)
+        {
+            compiler = new CSharpCompiler(assemblyName);
+        }
+        #endregion
+
+        public Task Start()
+        {
+            // TODO: 開始した時に行う処理
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            _botManager?.Dispose();
+            compiler.Dispose();
+        }
+    }
+}
---
+++ BotManager.Engine/IBotMechanism.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotManager.Engine
+{
+    /// <summary>
+    /// BotManager機関インターフェース
+    /// </summary>
+    public interface IBotMechanism : IDisposable
+    {
+        /// <summary>
+        /// Bot機能を開始します。
+        /// </summary>
+        Task Start();
+    }
+}
