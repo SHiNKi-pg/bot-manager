74a70124529bb3e551395f315430a343d3260c78
SHiNKi <shinki.5ktri@gmail.com>
2024-01-07 04:03:44 +0900

    スクリプトコンパイル用ライブラリ設定

--- BotManager.Engine/Compiler.cs
+++ BotManager.Engine/Compiler.cs
@@ -1,10 +1,27 @@
-﻿using BotManager.Service.Compiler;
+﻿using BotManager.Common;
+using BotManager.Common.Web;
+using BotManager.Database;
+using BotManager.Notifiers.EarthquakeMonitor;
+using BotManager.Reactive;
+using BotManager.Service.Compiler;
+using BotManager.Service.Discord;
+using BotManager.Service.Misskey;
+using BotManager.Service.Twitter;
+using Discord;
+using Discord.Rest;
+using Discord.WebSocket;
+using LinqToTwitter;
 using Microsoft.CodeAnalysis.CSharp;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Reactive.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Websocket.Client;
 
 namespace BotManager.Engine
 {
@@ -12,6 +29,32 @@ namespace BotManager.Engine
     {
         public Compiler(string assemblyName) : base(assemblyName, LanguageVersion.Latest)
         {
+            // スクリプトで使用しているクラスを正しく読み取れるようにする
+            Import<Regex>();
+            Import<Mutex>();
+            Import<Task>();
+            Import<IBotManager>();
+            Import<IEEWInfo>();
+            Import<IDisposable>();
+            Import(typeof(Encoding));
+            Import(typeof(Observable));
+            Import(typeof(Enumerable));
+            Import(typeof(KeyValuePair));
+            Import(typeof(BotDatabase));
+            Import(typeof(HttpClientSingleton));
+            Import(typeof(System.Collections.Generic.List<>));
+            Import<IDisposalNotifier>();
+            Import(typeof(AppSettings));
+            Import<Tweet>();
+            Import<SocketMessage>();
+            Import<IChannel>();
+            Import<WebsocketClient>();
+            Import<IDiscordServiceClient>();
+            Import<ITwitterServiceClient>();
+            Import<IMisskeyApi>();
+            Import<IDbConnection>();
+            Import<Expression>();
+            Import<RestUserMessage>();
         }
     }
 }
