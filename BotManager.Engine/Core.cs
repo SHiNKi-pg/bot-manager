32dded199c3d51b2a64dd9cdbd2b23202ef7abf6
SHiNKi <shinki.5ktri@gmail.com>
2024-01-07 04:25:09 +0900

    BotMechanismインスタンス作成用クラス追加

---
+++ BotManager.Engine/Core.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotManager.Engine
+{
+    /// <summary>
+    /// Botマネージャー機関
+    /// </summary>
+    public static class Core
+    {
+        /// <summary>
+        /// Botマネージャー機関インスタンスを作成します。
+        /// </summary>
+        /// <param name="assembly">アセンブリ名</param>
+        /// <returns></returns>
+        public static IBotMechanism Create(string assembly)
+        {
+            return new BotMechanism(assembly);
+        }
+    }
+}
