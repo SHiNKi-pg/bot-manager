d243c9b2b1dc7270ba2c29d167f7f41182ef9043
SHiNKi <shinki.5ktri@gmail.com>
2024-01-08 02:11:13 +0900

    メインシステムログ出力

--- BotManager/Program.cs
+++ BotManager/Program.cs
@@ -10,21 +10,33 @@ namespace BotManager
     {
         static async Task Main(string[] args)
         {
-            using(Clock clock = new Clock())
-            using(var eewMonitor = EEWNotifier.Create(clock))
-            using(var botm = Core.Create<SubscriptionArguments>(new MyCompiler("botmanage.dll"),
-                (named, bm) => new()
-                {
-                    BotManager = bm,
-                    Clock = clock,
-                    EEWMonitor = eewMonitor,
-                    Logger = Log.GetLogger("SBSC_" + named.Id)
-                }))
+            var sysLog = Log.GetLogger("main");
+            sysLog.Info("BotManager Start");
+            try
             {
-                await botm.Start();
+                using (Clock clock = new Clock())
+                using (var eewMonitor = EEWNotifier.Create(clock))
+                using (var botm = Core.Create<SubscriptionArguments>(new MyCompiler("botmanage.dll"),
+                    (named, bm) => new()
+                    {
+                        BotManager = bm,
+                        Clock = clock,
+                        EEWMonitor = eewMonitor,
+                        Logger = Log.GetLogger("SBSC_" + named.Id)
+                    }))
+                {
+                    await botm.Start();
 
-                // TODO: 待機処理を作成する
-                Console.ReadLine();
+                    // TODO: 待機処理を作成する
+                    Console.ReadLine();
+                }
+
+                sysLog.Info("BotManager End");
+            }catch(Exception ex)
+            {
+                sysLog.Fatal(ex.Message);
+                sysLog.Fatal("BotManager Abort");
+                throw;
             }
         }
     }
