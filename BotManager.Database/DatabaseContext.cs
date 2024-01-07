c9692abdd8d56e7394c0605e9aeb32b7b3f689ac
SHiNKi <shinki.5ktri@gmail.com>
2024-01-08 02:49:43 +0900

    データベースログ出力

--- BotManager.Database/DatabaseContext.cs
+++ BotManager.Database/DatabaseContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BotManager.Common;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Storage;
 using System;
 using System.Collections.Generic;
@@ -13,17 +14,21 @@ namespace BotManager.Database
     {
         #region Private Field
         private string _connectionString;
+
+        private static readonly ILog Logger = Log.GetLogger("DB");
         #endregion
         #region Constructor
         public DatabaseContext(string connectionString)
         {
             this._connectionString = connectionString;
+            Logger.Debug("DBContext Created");
         }
         #endregion
 
         #region Overrides
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            Logger.Debug("Use Oracle");
             optionsBuilder.UseOracle(_connectionString);
         }
         #endregion
@@ -35,6 +40,26 @@ namespace BotManager.Database
         public Task<IDbContextTransaction> BeginTransactionAsync()
         {
             return base.Database.BeginTransactionAsync();
+        }
+
+        public override void Dispose()
+        {
+            Logger.Debug("DBContext Disposing");
+            base.Dispose();
+            Logger.Debug("DBContext Disposed");
+        }
+
+        public override ValueTask DisposeAsync()
+        {
+            try
+            {
+                Logger.Debug("DBContext Disposing Async");
+                return base.DisposeAsync();
+            }
+            finally
+            {
+                Logger.Debug("DBContext Disposed Async");
+            }
         }
         #endregion
     }
