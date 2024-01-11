8e315b95bf656a66177ed1dcd5e2e31e12a29ca4
SHiNKi <shinki.5ktri@gmail.com>
2024-01-12 02:13:50 +0900

    Reactiveテスト追加

---
+++ Notifiers.Test/ReactiveTest.cs
@@ -0,0 +1,42 @@
+﻿using BotManager.Reactive;
+using Microsoft.Reactive.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace Notifiers.Test
+{
+    public class ReactiveTest
+    {
+        private readonly ITestOutputHelper output;
+
+        public ReactiveTest(ITestOutputHelper helper)
+        {
+            this.output = helper;
+        }
+
+        [Fact(DisplayName = "Interlocked Disposal Subscription")]
+        public async Task InterlockDisposingTest()
+        {
+            var numobs = Observable
+                .Interval(TimeSpan.FromSeconds(1))
+                .GroupBy(n => n % 10)
+                ;
+
+            var subscription = numobs.InterlockedSubscribe((g, d) =>
+            {
+                // Disposeされたことを受け取れるかテスト
+                g
+                .TakeUntil(d)
+                .Finally(() => output.WriteLine("Disposing"))
+                .Subscribe(n => output.WriteLine("{0}-{1}", g.Key, n));
+            });
+            await Task.Delay(25000);
+            subscription.Dispose();
+        }
+    }
+}
