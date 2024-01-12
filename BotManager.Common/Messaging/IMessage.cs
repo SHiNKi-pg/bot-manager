f8d517525ce612f0aad658e4e6743272da80b3cb
SHiNKi <shinki.5ktri@gmail.com>
2024-01-12 21:32:56 +0900

    共通メッセージインターフェース追加( #47 )

---
+++ BotManager.Common/Messaging/IMessage.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotManager.Common.Messaging
+{
+    /// <summary>
+    /// メッセージインターフェース
+    /// </summary>
+    public interface IMessage
+    {
+        /// <summary>
+        /// メッセージ内容
+        /// </summary>
+        string Content { get; }
+
+        /// <summary>
+        /// 受信日時
+        /// </summary>
+        DateTime ReceivedTime { get; }
+    }
+
+    /// <summary>
+    /// 返信可能メッセージインターフェース
+    /// </summary>
+    public interface IReplyableMessage : IMessage
+    {
+        /// <summary>
+        /// このメッセージに対して返信します。
+        /// </summary>
+        /// <param name="content">返信メッセージ</param>
+        /// <returns></returns>
+        Task<IReplyableMessage> Reply(string content);
+    }
+
+    /// <summary>
+    /// 削除可能メッセージインターフェース
+    /// </summary>
+    public interface IDeletableMessage : IMessage
+    {
+        /// <summary>
+        /// このメッセージを削除します。
+        /// </summary>
+        /// <returns></returns>
+        Task Delete();
+    }
+}
