a6d761650b67c3c2f87723c34dbb37d823dfc266
SHiNKi <shinki.5ktri@gmail.com>
2024-01-14 03:09:08 +0900

    ソースコードの追加、参照のインポートを別インターフェースに分離

--- BotManager.Service.Compiler/IPrecompilable.cs
+++ BotManager.Service.Compiler/IPrecompilable.cs
@@ -11,7 +11,7 @@ namespace BotManager.Service.Compiler
     /// <summary>
     /// コンパイルの結果のみ通知する機能を提供するインターフェース
     /// </summary>
-    public interface IPrecompilable
+    public interface IPrecompilable : ISourceIncluding
     {
         /// <summary>
         /// 現在の設定で試験的にコンパイルを実行し、結果を通知します。このメソッドではコンパイルしたアセンブリは通知されません。通常は<seealso cref="ICompiler.Compile"/>を実行する前に呼び出します。
---
+++ BotManager.Service.Compiler/ISourceIncluding.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotManager.Service.Compiler
+{
+    /// <summary>
+    /// ソースコードやインポートする型をコンパイルに通知するインターフェース
+    /// </summary>
+    public interface ISourceIncluding
+    {
+        /// <summary>
+        /// ソースコードをクリアします。
+        /// </summary>
+        void ClearSources();
+
+        /// <summary>
+        /// 指定した型をインポートします。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        void Import<T>();
+
+        /// <summary>
+        /// 指定したファイルから型をインポートします。
+        /// </summary>
+        /// <param name="filePath"></param>
+        void Import(string filePath);
+
+        /// <summary>
+        /// 指定した型をインポートします。
+        /// </summary>
+        /// <param name="type"></param>
+        void Import(Type type);
+
+        /// <summary>
+        /// インポート情報をクリアします。
+        /// </summary>
+        void ClearImports();
+
+        /// <summary>
+        /// ソースコードを追加します。
+        /// </summary>
+        /// <param name="source"></param>
+        void AddSource(string source);
+
+        /// <summary>
+        /// ソースファイルを追加します。
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <param name="encoding"></param>
+        /// <param name="usingBOM"></param>
+        /// <returns></returns>
+        Task AddSourceFile(string filePath, Encoding encoding, bool usingBOM);
+    }
+}
