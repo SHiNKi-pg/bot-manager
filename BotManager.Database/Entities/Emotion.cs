f8f1e737be59ae70fb398ef415347440c6e32738
SHiNKi <shinki.5ktri@gmail.com>
2024-01-13 17:20:30 +0900

    Emotionクラス追加

---
+++ BotManager.Database/Entities/Emotion.cs
@@ -0,0 +1,61 @@
+﻿using BotManager.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotManager.Database.Entities
+{
+    /// <summary>
+    /// TBL_EMOTIONテーブル
+    /// </summary>
+    [Table("TBL_EMOTION")]
+    [PrimaryKey(nameof(Id))]
+    public class Emotion
+    {
+        /// <summary>
+        /// 感情増分ID
+        /// </summary>
+        [Column("EMOTIONID")]
+        public ulong Id { get; }
+
+        /// <summary>
+        /// Bot ID
+        /// </summary>
+        [Column("BOTID")]
+        public required string BotId { get; set; }
+
+        /// <summary>
+        /// ユーザーID
+        /// </summary>
+        [Column("USERID")]
+        public required ulong UserId { get; set; }
+
+        /// <summary>
+        /// 感情値の増分
+        /// </summary>
+        [Column("EMOTIONVALUE")]
+        public required int Value { get; set; }
+
+        /// <summary>
+        /// データ作成日時
+        /// </summary>
+        [Column("CREATEDAT")]
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        /// <summary>
+        /// Bot
+        /// </summary>
+        [ForeignKey(nameof(BotId))]
+        public Bot? Bot { get; init; }
+
+        /// <summary>
+        /// User
+        /// </summary>
+        [ForeignKey(nameof(UserId))]
+        public User? User { get; init; }
+    }
+}
---
+++ BotManager.Database/Entities/EmotionTotal.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotManager.Database.Entities
+{
+    /// <summary>
+    /// MV_EMOTIONマテリアライズドビュー
+    /// </summary>
+    [Table("MV_EMOTION")]
+    [Keyless]
+    public class EmotionTotal
+    {
+        /// <summary>
+        /// Bot ID
+        /// </summary>
+        [Column("BOTID")]
+        public string BotId { get; init; } = "";
+
+        /// <summary>
+        /// ユーザーID
+        /// </summary>
+        [Column("USERID")]
+        public ulong UserId { get; init; }
+
+        /// <summary>
+        /// 感情の合計値
+        /// </summary>
+        [Column("EMOTIONSUM")]
+        public int TotalValue { get; init; }
+        
+
+        /// <summary>
+        /// Bot
+        /// </summary>
+        [ForeignKey(nameof(BotId))]
+        public Bot? Bot { get; init; }
+
+        /// <summary>
+        /// User
+        /// </summary>
+        [ForeignKey(nameof(UserId))]
+        public User? User { get; init; }
+    }
+}
