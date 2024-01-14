e8d9a6ac8b00beb9b46ca5a0432dddc755476d0d
SHiNKi <shinki.5ktri@gmail.com>
2024-01-14 14:43:04 +0900

    Like拡張メソッド追加

---
+++ BotManager.Common/Utility/StringExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BotManager.Common.Utility
+{
+    /// <summary>
+    /// <see cref="string"/>拡張メソッド定義
+    /// </summary>
+    public static class StringExtensions
+    {
+        #region Like
+        /// <summary>
+        /// この文字列が指定した正規表現パターンにマッチするかどうか返します。
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="pattern">正規表現パターン</param>
+        /// <returns>マッチする場合 true、それ以外は false。</returns>
+        public static bool Like(this string text, [StringSyntax(StringSyntaxAttribute.Regex)] string pattern)
+        {
+            Regex regex = new Regex(pattern);
+            Match match = regex.Match(text);
+            return match.Success;
+        }
+
+        /// <summary>
+        /// この文字列が指定した正規表現パターンにマッチするかどうか返します。
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="pattern">正規表現パターン</param>
+        /// <param name="options">オプション</param>
+        /// <returns>マッチする場合 true、それ以外は false。</returns>
+        public static bool Like(this string text, [StringSyntax(StringSyntaxAttribute.Regex)] string pattern, RegexOptions options)
+        {
+            Regex regex = new Regex(pattern, options);
+            Match match = regex.Match(text);
+            return match.Success;
+        }
+        #endregion
+    }
+}
