be582a493c93a2e19f2c2d9d731aca7ff2a67db1
SHiNKi <shinki.5ktri@gmail.com>
2024-01-14 16:11:14 +0900

    Random.Choice拡張メソッド追加 指定したコレクションから要素をランダムで取り出す

---
+++ BotManager.Common/Utility/RandomExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotManager.Common.Utility
+{
+    /// <summary>
+    /// <see cref="Random"/>拡張メソッド定義
+    /// </summary>
+    public static class RandomExtensions
+    {
+        /// <summary>
+        /// 指定したコレクションの中からランダムで要素を1つ選択して返します。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="random"></param>
+        /// <param name="items"></param>
+        /// <returns></returns>
+        public static T Choice<T>(this Random random, params T[] items)
+        {
+            int count = items.Length;
+            return items[random.Next(0, count)];
+        }
+
+        /// <summary>
+        /// 指定したコレクションの中からランダムで要素を1つ選択して返します。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="random"></param>
+        /// <param name="items"></param>
+        /// <returns></returns>
+        public static T Choice<T>(this Random random, IEnumerable<T> items)
+        {
+            return random.Choice(items.ToArray());
+        }
+    }
+}
