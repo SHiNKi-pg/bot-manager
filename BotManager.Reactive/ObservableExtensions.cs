a5dff0c500f50ed9c52dde0af230c473a1d0551a
SHiNKi <shinki.5ktri@gmail.com>
2024-01-14 23:38:35 +0900

    IObservable拡張メソッド定義クラス追加

---
+++ BotManager.Reactive/ObservableExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotManager.Reactive
+{
+    /// <summary>
+    /// <see cref="IObservable{T}"/>拡張メソッド定義
+    /// </summary>
+    public static class ObservableExtensions
+    {
+
+    }
+}
