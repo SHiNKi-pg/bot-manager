b325564867c62fb36f3214ac3f3dd9fd68dd59d9
SHiNKi <shinki.5ktri@gmail.com>
2024-01-16 03:11:41 +0900

    Race 拡張メソッド追加

--- BotManager.Common/Extensions/BotManagerExtensions.cs
+++ BotManager.Common/Extensions/BotManagerExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,5 +12,33 @@ namespace BotManager.Common.Extensions
     /// </summary>
     public static class BotManagerExtensions
     {
+        #region Race
+        /// <summary>
+        /// この Botコレクションのうち、指定したイベントが一番早く来た Botの通知が後続に流れるようにします。
+        /// </summary>
+        /// <typeparam name="TBot"></typeparam>
+        /// <typeparam name="TSource"></typeparam>
+        /// <param name="bots"></param>
+        /// <param name="observableSelector">イベント通知</param>
+        /// <returns></returns>
+        public static IObservable<IValueSender<TBot, TSource>> Race<TBot, TSource>(this IEnumerable<TBot> bots, Func<TBot, IObservable<TSource>> observableSelector) where TBot : IBot
+        {
+            var observables = bots.Select(b => observableSelector(b).Select(t => new _Sender<TBot, TSource>(b, t)));
+            return Observable.Amb(observables);
+        }
+
+        private class _Sender<TSender, TValue> : IValueSender<TSender, TValue>
+        {
+            public TSender Sender { get; }
+            public TValue Value { get; }
+
+            public _Sender(TSender sender, TValue value)
+            {
+                this.Sender = sender;
+                this.Value = value;
+            }
+        }
+
+        #endregion
     }
 }
