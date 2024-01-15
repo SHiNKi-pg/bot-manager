0a4dfc65688d58434f65b00e22a44eaf4ea8d877
SHiNKi <shinki.5ktri@gmail.com>
2024-01-16 02:05:58 +0900

    Restart API追加

--- BotManagerWebAPI/Controllers/CompilerController.cs
+++ BotManagerWebAPI/Controllers/CompilerController.cs
@@ -61,5 +61,58 @@ namespace BotManagerWebAPI.Controllers
                 return StatusCode(500, ex.Message);
             }
         }
+
+        [HttpPost("restart")]
+        public async Task<ActionResult> Restart()
+        {
+            try
+            {
+                var compiler = Entry.BotMechanism.PreCompiler;
+                using (var repos = Entry.BotMechanism.GetScriptRepositry())
+                {
+                    repos.Checkout(AppSettings.Script.BranchName);
+
+                    // C#ソースファイルの再設定
+                    await compiler.SetSourceFilesAsync(repos.LocalDirectory.DirectoryInfo.EnumerateFiles("*.cs", SearchOption.AllDirectories));
+                    var compilerobs = compiler.Precompile().Publish();
+
+                    bool success = false;
+                    IEnumerable<Diagnostic> messages = Enumerable.Empty<Diagnostic>();
+
+                    compilerobs.OfType<ICompileSuccessed>().Subscribe(r => success = r.Success);
+                    compilerobs.OfType<ICompileMessage>().Subscribe(c => messages = c.Messages);
+
+                    using (compilerobs.Connect())
+                    {
+                        await compilerobs.Count();
+                    }
+
+                    if (!success)
+                    {
+                        // コンパイル失敗
+                        return StatusCode(400, new
+                        {
+                            Result = success,
+                            Messages = messages
+                                .Where(mes => mes.Severity != DiagnosticSeverity.Hidden)
+                                .Select(mes => new
+                                {
+                                    Severity = mes.Severity,
+                                    Id = mes.Id,
+                                    Line = mes.Location.GetLineSpan().StartLinePosition.Line,
+                                    Message = mes.GetMessage(),
+                                })
+                        });
+                    }
+                    // コンパイル成功
+                    await Entry.BotMechanism.CompileSources();
+                    return StatusCode(200);
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
     }
 }
