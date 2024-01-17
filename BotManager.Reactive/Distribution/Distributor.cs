143879ee1986b34b13bfed2dd53d72ca191c5ec7
SHiNKi <shinki.5ktri@gmail.com>
2024-01-17 20:24:33 +0900

    分配インターフェース追加

---
+++ BotManager.Reactive/Distribution/Distributor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotManager.Reactive.Distribution
+{
+    /// <summary>
+    /// 値分配インターフェース
+    /// </summary>
+    public interface IDistributor<T> : IObserver<T>, IDisposable
+    {
+        /// <summary>
+        /// <seealso cref="IDistributee{T}"/>オブジェクトを追加します。
+        /// </summary>
+        /// <param name="distributee"></param>
+        /// <returns></returns>
+        IDisposable Add(IDistributee<T> distributee);
+    }
+}
